b592f82df44a83d4046a66ab71da18530b9bb700
andrei-iosif <andrei.iosif24@gmail.com>
2023-05-20 12:48:22 +0300

    RAFT ensemble training

--- raft/train_standard.sh
+++ raft/train_standard.sh
@@ -12,11 +12,11 @@
 #     --gpus 0 --num_steps 5000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
 #     --seed 0 --uncertainty --debug_iter --log_variance
 
-python -u train.py --name raft_chairs_100_sample_seed_0_nll_loss_v2_variance_ELU \
-    --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_100_sample_seed_0_nll_loss_v2_variance_ELU \
-    --num_overfit_samples 100 --val_freq 1000 --stage chairs --validation chairs \
-    --gpus 0 --num_steps 5000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
-    --seed 0 --uncertainty --debug_iter
+# python -u train.py --name raft_chairs_100_sample_seed_0_nll_loss_v2_variance_ELU \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_100_sample_seed_0_nll_loss_v2_variance_ELU \
+#     --num_overfit_samples 100 --val_freq 1000 --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 5000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 0 --uncertainty --debug_iter
 
 # python -u train.py --name raft_chairs_100_sample_seed_0_nll_loss_v1_log_variance_residual \
 #     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_100_sample_seed_0_nll_loss_v2_variance \
@@ -24,18 +24,74 @@ python -u train.py --name raft_chairs_100_sample_seed_0_nll_loss_v2_variance_ELU
 #     --gpus 0 --num_steps 5000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
 #     --seed 0 --uncertainty --debug_iter --log_variance --residual_variance
 
-python -u train.py --name raft_chairs_100_sample_seed_0_nll_loss_v2_variance_residual_ELU \
-    --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_100_sample_seed_0_nll_loss_v2_variance_residual_ELU \
-    --num_overfit_samples 100 --val_freq 1000 --stage chairs --validation chairs \
-    --gpus 0 --num_steps 5000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
-    --seed 0 --uncertainty --debug_iter --residual_variance
+# python -u train.py --name raft_chairs_100_sample_seed_0_nll_loss_v2_variance_residual_ELU \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_100_sample_seed_0_nll_loss_v2_variance_residual_ELU \
+#     --num_overfit_samples 100 --val_freq 1000 --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 5000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 0 --uncertainty --debug_iter --residual_variance
+
+
+# python -u train.py --name raft_chairs_seed_0_nll_loss_v1_log_variance \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_0_nll_loss_v1_log_variance \
+#     --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 0 --uncertainty --debug_iter --log_variance
+
+# python -u train.py --name raft_chairs_seed_0_nll_loss_v2_residual_variance \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_0_nll_loss_v2_residual_variance \
+#     --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 0 --uncertainty --debug_iter --residual_variance
+
+# python -u train.py --name raft_chairs_seed_42_nll_loss_v1_log_variance \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_42_nll_loss_v1_log_variance \
+#     --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 42 --uncertainty --debug_iter --log_variance
+
+# python -u train.py --name raft_chairs_seed_42_nll_loss_v2_residual_variance \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_42_nll_loss_v2_residual_variance \
+#     --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 42 --uncertainty --debug_iter --residual_variance
+
+# python -u train.py --name raft_chairs_seed_1234_nll_loss_v1_log_variance \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_1234_nll_loss_v1_log_variance \
+#     --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 1234 --uncertainty --debug_iter --log_variance
+
+# python -u train.py --name raft_chairs_seed_1234_nll_loss_v2_residual_variance \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_1234_nll_loss_v2_residual_variance \
+#     --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 1234 --uncertainty --debug_iter --residual_variance
 
 ## Baseline experiments
 # python -u train.py --name train_raft_vkitti_seed_0_mixed --restore_ckpt=../models/raft_original_models/raft-things.pth --checkpoint_out=../checkpoints/raft_vkitti/train_raft_vkitti_seed_0_mixed --stage virtual_kitti --validation kitti --gpus 0 --num_steps 100000 --batch_size 6 --lr 0.0001 --image_size 288 960 --wdecay 0.0001 --seed 0 --mixed_precision
 # python -u train.py --name train_raft_vkitti_seed_42_mixed --restore_ckpt=../models/raft_original_models/raft-things.pth --checkpoint_out=../checkpoints/raft_vkitti/train_raft_vkitti_seed_42_mixed --stage virtual_kitti --validation kitti --gpus 0 --num_steps 100000 --batch_size 6 --lr 0.0001 --image_size 288 960 --wdecay 0.0001 --seed 42 --mixed_precision
-#python -u train.py --name train_raft_virtual_kitti_seed_1234_mixed --restore_ckpt=../models/raft_original_models/raft-things.pth --checkpoint_out=../checkpoints/raft_virtual_kitti/train_raft_virtual_kitti_seed_1234_mixed --stage virtual_kitti --validation kitti --gpus 0 --num_steps 100000 --batch_size 6 --lr 0.0001 --image_size 288 960 --wdecay 0.0001 --seed 1234 --mixed_precision
+# python -u train.py --name train_raft_virtual_kitti_seed_1234_mixed --restore_ckpt=../models/raft_original_models/raft-things.pth --checkpoint_out=../checkpoints/raft_virtual_kitti/train_raft_virtual_kitti_seed_1234_mixed --stage virtual_kitti --validation kitti --gpus 0 --num_steps 100000 --batch_size 6 --lr 0.0001 --image_size 288 960 --wdecay 0.0001 --seed 1234 --mixed_precision
 
 # python -u train.py --name train_raft_viper_vkitti_seed_1234_mixed --restore_ckpt=../checkpoints/raft_viper/train_raft_viper_seed_1234_mixed/raft-viper.pth --checkpoint_out=../checkpoints/raft_viper_vkitti/train_raft_viper_vkitti_seed_1234_mixed --stage virtual_kitti --validation kitti --gpus 0 --num_steps 100000 --batch_size 6 --lr 0.0001 --image_size 288 960 --wdecay 0.0001 --seed 1234 --mixed_precision
+
+
+python -u train.py --name raft_chairs_seed_1234 \
+    --checkpoint_out=../checkpoints/raft_baseline/raft_chairs_seed_1234 \
+    --stage chairs --validation chairs \
+    --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+    --seed 1234 --debug_iter
+
+python -u train.py --name raft_chairs_seed_0 \
+    --checkpoint_out=../checkpoints/raft_baseline/raft_chairs_seed_0 \
+    --stage chairs --validation chairs \
+    --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+    --seed 0 --debug_iter
+
+python -u train.py --name raft_chairs_seed_42 \
+    --checkpoint_out=../checkpoints/raft_baseline/raft_chairs_seed_42 \
+    --stage chairs --validation chairs \
+    --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+    --seed 42 --debug_iter
 
 
 ## DEBUG
