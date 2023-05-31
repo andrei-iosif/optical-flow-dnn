a5ae0c84e608130ac589ed983e335e2e0e858019
andrei-iosif <andrei.iosif24@gmail.com>
2023-05-31 18:49:32 +0300

    training complete

--- raft/train_standard.sh
+++ raft/train_standard.sh
@@ -105,11 +105,23 @@
 #     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
 #     --seed 42 --debug_iter --dropout 0.2
 
-python -u train.py --name raft_chairs_seed_42_nll_loss_v2_residual_variance \
-    --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_42_nll_loss_v2_residual_variance \
-    --stage chairs --validation chairs \
-    --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
-    --seed 42 --uncertainty --debug_iter --residual_variance
+# python -u train.py --name raft_chairs_seed_0_uncertainty_v3 \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_0_uncertainty_v3 \
+#     --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 0 --uncertainty --debug_iter --log_variance --flow_variance_last_iter
+
+# python -u train.py --name raft_chairs_seed_42_uncertainty_v3 \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_42_uncertainty_v3 \
+#     --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 42 --uncertainty --debug_iter --log_variance --flow_variance_last_iter
+
+# python -u train.py --name raft_chairs_seed_1234_uncertainty_v3 \
+#     --checkpoint_out=../checkpoints/raft_uncertainty/raft_chairs_seed_1234_uncertainty_v3 \
+#     --stage chairs --validation chairs \
+#     --gpus 0 --num_steps 100000 --batch_size 10 --lr 0.0004 --image_size 368 496 --wdecay 0.0001 \
+#     --seed 1234 --uncertainty --debug_iter --log_variance --flow_variance_last_iter
 
 # RAFT Semantic
 # python -u train.py \
